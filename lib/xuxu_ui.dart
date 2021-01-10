f60651afaa85de8eb1ed482ac607c8cd49d246c8
Ercan Garip <ercngp@gmail.com>
2021-01-10 15:40:08 +0300

    updated main

--- lib/xuxu_ui.dart
+++ lib/xuxu_ui.dart
@@ -5,6 +5,7 @@ export 'src/extensions/center.dart';
 export 'src/extensions/padding.dart';
 export 'package:xuxu_ui/src/widgets/xutext/XuText.dart';
 export 'src/widgets/xubox/xubox.dart';
+export 'src/extensions/flexible.dart';
 export 'src/widgets/xubox/xubox_decoration.dart';
 export 'src/extensions/gesture.dart';
 export 'src/widgets/xuvstack/XuVStack.dart';
@@ -15,7 +16,3 @@ export 'package:xuxu_ui/src/extensions/unique.dart';
 export 'src/widgets/xuhstack/XuHStack.dart';
 export 'src/widgets/xulistview/XuListView_builder.dart';
 
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
